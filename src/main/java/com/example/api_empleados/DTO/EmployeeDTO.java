5efc67244684763a4ed94176ab42d272a72876ab
Juanandress <sjuanandres94@gmail.com>
2025-07-08 13:27:13 -0500

    Creación de DTO's para Employee y Travel

--- src/main/java/com/example/api_empleados/DTO/EmployeeDTO.java
+++ src/main/java/com/example/api_empleados/DTO/EmployeeDTO.java
@@ -1,12 +1,14 @@
-package com.example.api_empleados.model;
+package com.example.api_empleados.DTO;
 import java.util.*;
+import com.example.api_empleados.model.TravelEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data@AllArgsConstructor@NoArgsConstructor
-public class EmployeeEntity {
+public class EmployeeDTO {
     private String id;
     private String nombre;
     private ArrayList<TravelEntity> viajes;
+    
 }
