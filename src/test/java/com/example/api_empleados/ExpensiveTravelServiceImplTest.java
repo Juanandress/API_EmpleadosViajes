01319d540faa2295ad94ff807ce458192e3c1fdb
Juanandress <sjuanandres94@gmail.com>
2025-07-08 23:26:34 -0500

    Corrección de mensajes y ajuste en pruebas

--- src/test/java/com/example/api_empleados/ExpensiveTravelServiceImplTest.java
+++ src/test/java/com/example/api_empleados/ExpensiveTravelServiceImplTest.java
@@ -19,4 +19,29 @@ public class ExpensiveTravelServiceImplTest {
         assertNotNull(report.getResumenGastosPorEmpleado(), "La lista de empleados no debe ser null");
         assertTrue(report.getResumenGastosPorEmpleado().size() > 0, "Debe haber al menos un empleado en el reporte");
     }
+
+    @Test
+    void testEmpleadosOrdenadosAlfabeticamente() {
+        TravelReportDTO report = service.getReport();
+        for (int i = 1; i < report.getResumenGastosPorEmpleado().size(); i++) {
+            String antes = report.getResumenGastosPorEmpleado().get(i-1).getNombre();
+            String actual = report.getResumenGastosPorEmpleado().get(i).getNombre();
+            assertTrue(antes.compareToIgnoreCase(actual) <= 0, "Los empleados deben estar ordenados alfabéticamente por nombre");
+        }
+    }
+    
+    @Test
+    void testLogicaQuienAsumeGasto() {
+        TravelReportDTO report = service.getReport();
+        report.getResumenGastosPorEmpleado().forEach(empleado -> {
+            empleado.getGastoPorMes().forEach(gasto -> {
+                if (gasto.getTotalMes() > 1000000) {
+                    assertEquals("SURA", gasto.getAsume(), "El gasto mayor a 1,000,000 debe ser asumido por SURA");
+                } else {
+                    assertEquals("Empleado", gasto.getAsume(), "El gasto menor o igual a 1,000,000 debe ser asumido por el Empleado");
+                }
+            });
+        });
+    }
+
 }
